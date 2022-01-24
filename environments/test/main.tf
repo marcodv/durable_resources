44b90e48dc61dbf04273dbdaf36ab69388c557f6
Marco di Vincenzo <marco@noah.energy>
2022-01-24 16:32:31 +0100

    Add S3 module

---
+++ environments/test/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-durable-test-env"
+    key            = "terraform-state-durable-test-env/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "tfStateLockingDurableTestEnv"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Resource    = "Durable"
+    }
+  }
+}
